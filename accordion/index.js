e5887b764498aa2ecc6084ff2fcdb3e29d94a9cd
Rodriguez, Raul Ivan <raulivan.rodriguez@solera.com>
2025-02-18 17:52:45 -0600

    Implement accordion animation and improve structure with data attributes

--- accordion/index.js
+++ accordion/index.js
@@ -1,1 +1,30 @@
-console.log('accordion/index.js')
+const fadeIn = [
+    { display: 'inline-block', offset: 0},
+    { transform: 'scaleY(1)', display: 'inline-block', offset: 1}
+]
+
+const fadeOut = [
+    { transform: 'scaleY(0)', offset: 1},
+    { display: 'none', offset: 1},
+]
+const animationOptions = {
+    duration: 300,
+    easing: 'ease-in-out',
+    fill: 'forwards'
+}
+const handleClickArrow = (event) => {
+    const arrow = event.target
+    const id = arrow.getAttribute('data-id')
+    const spanArrow = document.getElementById(id)
+    const accordionContent = document.querySelector(`div[data-id="${id}"]`)
+
+    if( spanArrow.classList.contains('inverted') ){
+        spanArrow.classList.remove('inverted')
+        accordionContent.animate(fadeOut, animationOptions)
+        return
+    } else {
+        spanArrow.classList.add('inverted')
+        accordionContent.animate(fadeIn, animationOptions)
+        return
+    }
+}
